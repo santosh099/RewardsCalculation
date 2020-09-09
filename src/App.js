45b6ffd9c883ca8d9241fb565c37c57b0eb045d0
Santosh Jeebula <santoshjeebula@Santoshs-MacBook-Pro.local>
2020-09-09 16:19:04 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { dataSet } from './utils/dataSet';
 
 function App() {
+
+
+  const totalRewardPoints = (updatedTransactions) => {
+    const rewardPoints = updatedTransactions.reduce((value,transaction) => {
+      if (transaction.transactionAmount > 100) {
+        return value + (transaction.transactionAmount - 100 ) * 2 + 50; 
+      } else if(transaction.transactionAmount > 50 && transaction.transactionAmount <= 100) {
+        return value + (transaction.transactionAmount - 50);
+      } 
+      return value;
+    }, 0)
+    return rewardPoints;
+  }
+
+  const rewardPointsByMonth = (accountId, month) => {
+    const filteredTransactions = dataSet.filter((transaction) => {
+      const date = new Date(transaction.date);
+      if(transaction.accountId === accountId && date.getMonth() + 1 === month) {
+        return transaction;
+      }
+    });
+    return totalRewardPoints(filteredTransactions);
+  }
+
+  const renderTableRows = () => {
+    let uniqueAccounts = [...new Set(dataSet.map(transaction => transaction.accountId))];
+    return uniqueAccounts.map((accountId, index)  => {
+      return  (<tr key = {index}>
+              <td>{accountId}</td>
+              <td>{rewardPointsByMonth(accountId, 1)}</td>
+              <td>{rewardPointsByMonth(accountId, 2)}</td>
+              <td>{rewardPointsByMonth(accountId, 3)}</td>
+              <td>{rewardPointsByMonth(accountId , 1) + rewardPointsByMonth(accountId, 2) + rewardPointsByMonth(accountId, 3)}</td>
+           </tr>);
+    });
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style = {{textAlign:"center"}}>Rewards Application</h1>
+      <table className="container">
+        <thead>
+          <tr>
+            <th>AccountId</th>
+            <th>Jan</th>
+            <th>Feb</th>
+            <th>March</th>
+            <th>Total Reward Points</th>
+          </tr>
+        </thead>
+        <tbody>
+          {renderTableRows()}
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/utils/dataSet.js
@@ -0,0 +1,125 @@
+export const dataSet = [
+    {
+        transactionId: "0000000101",
+        accountId: "A0001",
+        firstName: "Michale",
+        lastName: "Robbin",
+        transactionAmount: 12,
+        date: "2020-01-15"
+    },
+    {
+        transactionId: "0000000102",
+        accountId: "A0001",
+        firstName: "Michale",
+        lastName: "Robbin",
+        transactionAmount: 65,
+        date: "2020-01-18"
+    },
+    {
+        transactionId: "0000000103",
+        accountId: "A0001",
+        firstName: "Michale",
+        lastName: "Robbin",
+        transactionAmount: 134,
+        date: "2020-02-09"
+    },
+    {
+        transactionId: "0000000104",
+        accountId: "A0001",
+        firstName: "Michale",
+        lastName: "Robbin",
+        transactionAmount: 85,
+        date: "2020-02-25"
+    },
+    {
+        transactionId: "0000000105",
+        accountId: "A0001",
+        firstName: "Michale",
+        lastName: "Robbin",
+        transactionAmount: 135,
+        date: "2020-03-22"
+    },
+    {
+        transactionId: "0000000106",
+        accountId: "A0002",
+        firstName: "Carlos",
+        lastName: "Snares",
+        transactionAmount: 75,
+        date: "2020-01-16"
+    },
+    {
+        transactionId: "0000000107",
+        accountId: "A0002",
+        firstName: "Carlos",
+        lastName: "Snares",
+        transactionAmount: 35,
+        date: "2020-01-17"
+    },
+    {
+        transactionId: "0000000108",
+        accountId: "A0002",
+        firstName: "Carlos",
+        lastName: "Snares",
+        transactionAmount: 90,
+        date: "2020-02-26"
+    },
+    {
+        transactionId: "0000000109",
+        accountId: "A0002",
+        firstName: "Carlos",
+        lastName: "Snares",
+        transactionAmount: 50,
+        date: "2020-02-06"
+    },
+    {
+        transactionId: "0000000110",
+        accountId: "A0002",
+        firstName: "Carlos",
+        lastName: "Snares",
+        transactionAmount: 187,
+        date: "2020-03-26"
+    },
+    {
+        transactionId: "0000000111",
+        accountId: "A0003",
+        firstName: "Enric",
+        lastName: "Dosio",
+        transactionAmount: 100,
+        date: "2020-01-16"
+    },
+    {
+        transactionId: "0000000112",
+        accountId: "A0003",
+        firstName: "Enric",
+        lastName: "Dosio",
+        transactionAmount: 123,
+        date: "2020-02-14"
+    },
+    {
+        transactionId: "0000000113",
+        accountId: "A0003",
+        firstName: "Enric",
+        lastName: "Dosio",
+        transactionAmount: 9,
+        date: "2020-03-06"
+    },
+    {
+        transactionId: "0000000114",
+        accountId: "A0003",
+        firstName: "Enric",
+        lastName: "Dosio",
+        transactionAmount: 128,
+        date: "2020-03-10"
+    },
+    {
+        transactionId: "0000000115",
+        accountId: "A0003",
+        firstName: "Enric",
+        lastName: "Dosio",
+        transactionAmount: 65,
+        date: "2020-03-26"
+    }
+    
+    
+    
+]
